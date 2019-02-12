4c0ae3aee8f62830739f1cee0c457601648e9a42
Kaitaku Takeda <katakeda32@gmail.com>
2019-02-11 17:37:07 -0800

    Remove unused code

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Installed!");
 })
-
-chrome.runtime.onMessage.addListener((request, sender, response) => {
-    console.log(request);
-    console.log(sender);
-    console.log(response);
-})
